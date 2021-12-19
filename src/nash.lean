e7afb6ca2c4b1f6d3e63a76961986be5a294f897
dipplestix <chrism315@gmail.com>
2021-12-18 23:07:42 -0500

    ready for submission

--- src/nash.lean
+++ src/nash.lean
@@ -158,24 +158,3 @@ begin
   exact x,
 end
 
-/-!
-To-dos:
-1) List the conditions for K fixed points and use the pris_dil_bounded and best_response_exists lemmas
-inside the NE lemma
--- Done in line 152-155
-
-2) Start working on the same things, but for mixed strategies
-I'm going to move this to a second file to not mess things up here
-  - Create a simplex class
-    -- didn't work out very well
-  - Switch from Action to Strategy (action is just strategy where ps are 1 or 0)
-    -- See nash_mixed.lean for the start of this
-  - Do the same for a game that requires mixed strategy
-    -- no particulat game was defined
-  - Probbaly won't finish this part, but think I can make some good progress on it
-
-  -- Other directions
-    - Try to prove pieces of fixed point theorem
-      -- Didn't have time
--/
-
